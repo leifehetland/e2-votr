504596d52d7212a78e40e2a9b439db68c3217a10
Leif Hetland <lehetland1@gmail.com>
2016-04-23 14:04:30 -0500

    concludes Apr 23 lesson

--- Votr.Tests/PollTests.cs
+++ Votr.Tests/PollTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Votr.Models;
+using Votr.DAL;
 
 namespace Votr.Tests
 {
@@ -17,6 +18,21 @@ namespace Votr.Tests
         {
             Poll p = new Poll();
             Assert.IsNotNull(p);
+        }
+
+        [TestMethod]
+        public void PollEnsureICanSaveAPoll()
+        {
+            //Arrange
+            VotrContext context = new VotrContext();
+            Poll p = new Poll();
+
+            //Act
+            context.Polls.Add(p);
+            context.SaveChanges();
+
+            //Assert
+            Assert.AreEqual(1, context.Polls.Find().PollId);
         }
     }
 }
